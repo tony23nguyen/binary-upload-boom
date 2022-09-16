3a593cbf96d9b879c3867b3e945e303b3fe3bfc4
tony23nguyen <96150979+tony23nguyen@users.noreply.github.com>
2022-09-15 20:56:24 -0700

    updated notes and config

---
+++ comment notes.js
@@ -0,0 +1,32 @@
+// Work flow for adding comments
+
+// ..... >
+
+
+
+//create comment model 
+
+//ul -> li loop to grab all comments
+
+// add comment form and action create comment
+
+// build out route to post comment in server.js 
+
+//new comment route === /comment, commentRoutes and require comment route -> new comment.js files in routes
+
+//comments controller require controllers comments
+
+//comment route only needs post no post, delete, get. /createComment/:id. comments controller
+
+//commentsController.createComment
+
+//in controllers?
+//req.body.comment
+
+//post:req.params.id
+
+//create comment model
+//grabbing post + grab comment id 
+//create comment form
+//new route, /comments router. post('/createComment:id, commentscontroller.createcomment)
+// use comment model and create new, redirect back to original comment
